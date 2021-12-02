3cd156d3ed4566cd409d86424d64b2443ee07244
Дмитрий М <mustaev-d@inbox.ru>
2021-12-02 16:28:34 +0300

    Zadanie_1_2 is done!

--- Zadanie_1/WinAnim/Form1.cs
+++ Zadanie_1/WinAnim/Form1.cs
@@ -1,11 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace WinAnim
@@ -15,6 +8,51 @@ namespace WinAnim
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void btnAW_BLEND_Click(object sender, EventArgs e)
+        {
+            // Скрываем окно
+            Hide();
+
+            // Запускаем анимацию.
+            // Второй параметр в скобках — время анимации в
+            // миллисекундах. 
+            WinAPIClass.AnimateWindow(
+                this,
+                3000,
+                WinAPIClass.AnimateWindowFlags.AW_ACTIVATE | WinAPIClass.AnimateWindowFlags.AW_BLEND);
+
+            // Отображаем кнопки после анимации
+            btnAW_BLEND.Invalidate();
+            btnHOR_AW_SLIDE.Invalidate();
+            btnCenter_AW_SLIDE.Invalidate();
+        }
+        private void btnHOR_AW_SLIDE_Click(object sender, EventArgs e)
+        {
+            Hide();
+
+            WinAPIClass.AnimateWindow(
+                this,
+                3000,
+                WinAPIClass.AnimateWindowFlags.AW_HOR_POSITIVE | WinAPIClass.AnimateWindowFlags.AW_SLIDE);
+
+            btnAW_BLEND.Invalidate();
+            btnHOR_AW_SLIDE.Invalidate();
+            btnCenter_AW_SLIDE.Invalidate();
+        }
+        private void btnCenter_AW_SLIDE_Click(object sender, EventArgs e)
+        {
+            Hide();
+
+            WinAPIClass.AnimateWindow(
+                this,
+                3000,
+                WinAPIClass.AnimateWindowFlags.AW_CENTER | WinAPIClass.AnimateWindowFlags.AW_SLIDE);
+
+            btnAW_BLEND.Invalidate();
+            btnHOR_AW_SLIDE.Invalidate();
+            btnCenter_AW_SLIDE.Invalidate();
         }
     }
 }
